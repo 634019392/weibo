00d83ba0d5fcb0bf2f9d6f2392d6cf4a6927cc36
ll <634019392@qq.com>
2020-04-28 12:48:08 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,40 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at', 'desc');
     }
+
+    // 粉丝（粉丝关系列表）
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 关注者（用户关注人列表）
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 判断用户 A 是否关注了用户 B
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
