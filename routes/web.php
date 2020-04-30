9e162cd22e82e1d840df9e735ae1f47c8f81cba6
ll <634019392@qq.com>
2020-04-30 11:50:21 +0000

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,8 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 // 发布和删除微博
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+// 关注用户
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+// 取消关注用户
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
